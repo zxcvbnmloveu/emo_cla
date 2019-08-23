3709bb1e362c2291bf06541a9004080290056932
zxcvbnmloveu <qyziwei@163.com>
2019-08-23 15:07:53 +0800

    建立不同通道和不同分段的deap数据集

--- convertdata2.py
+++ convertdata2.py
@@ -19,44 +19,40 @@ from scipy.stats import skew, kurtosis
 warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
 
 def convertData(adr = "C:\\Users\\zxcvb\\PycharmProjects\\new1\\data/features_1.dat" ):
+    # 保存到features_1文件里
+    # 标签值保存在label0等文件里， 用labelcalss0等文件二值化
     print("Program started"+"\n")
     fout_data = open(adr,'w')
-    fout_labels0 = open("C:\\Users\\zxcvb\\PycharmProjects\\new1\\data\labels_0.dat",'w')
-    fout_labels1 = open("C:\\Users\\zxcvb\\PycharmProjects\\new1\\data\labels_1.dat",'w')
-    fout_labels2 = open("C:\\Users\\zxcvb\\PycharmProjects\\new1\\data\labels_2.dat",'w')
-    fout_labels3 = open("C:\\Users\\zxcvb\\PycharmProjects\\new1\\data\labels_3.dat",'w')
-
     print("\n"+"Print Successful")
-    for i in range(32):
-    # for i in [i for i in range(32) if i != 1]:  #nUser #4, 40, 32, 40, 8064
-    # for i in [1]:  # nUser #4, 40, 32, 40, 8064
+    each_seg = 15
+    seg_num = 60 // each_seg
+    fs = 128
 
-        if(i%1 == 0):
-            if i < 10:
-                name = '%0*d' % (2,i+1)
-            else:
-                name = i+1
-        fname = "C:\\Users\\zxcvb\\PycharmProjects\\new1\\data_preprocessed_python\\s"+str(name)+".dat"     #C:/Users/lumsys/AnacondaProjects/Emo/
-        f = open(fname, 'rb')
-        x = pickle.load(f, encoding='latin1')
-        print(fname)
-        for tr in range(nTrial):
-            if(tr%1 == 0):
-                start = 128 * 3
-                # features = gsr_features
+    data = np.zeros((10,seg_num,32*nTrial,each_seg*fs))
+    print(data.shape)
+    for ch in range(10):
+        start = 128 * 3
+        for seg in range(seg_num):
 
-                # for fea in features:
-                #     fout_data.write(str(fea) + " ")
+            print("channel %d seg %d "%(ch,seg))
 
-                fout_labels0.write(str(x['labels'][tr][0]) + "\n")
-                fout_labels1.write(str(x['labels'][tr][1]) + "\n")
-                fout_labels2.write(str(x['labels'][tr][2]) + "\n")
-                fout_labels3.write(str(x['labels'][tr][3]) + "\n")
-                fout_data.write("\n")#40个特征换行
-    fout_labels0.close()
-    fout_labels1.close()
-    fout_labels2.close()
-    fout_labels3.close()
+            next = start + each_seg * fs
+            for i in range(32):
+                if(i%1 == 0):
+                    if i < 10:
+                        name = '%0*d' % (2,i+1)
+                    else:
+                        name = i+1
+                fname = "C:\\Users\\zxcvb\\PycharmProjects\\new1\\data_preprocessed_python\\s"+str(name)+".dat"     #C:/Users/lumsys/AnacondaProjects/Emo/
+                f = open(fname, 'rb')
+                x = pickle.load(f, encoding='latin1')
+                for tr in range(nTrial):
+                    if(tr%1 == 0):
+                        data[ch][seg][i*40 + tr][:] = x['data'][tr][ch][start:next]
+
+            start = next
+    np.save('./data/seg_data.npy', data)
+
     fout_data.close()
     print("\n"+"Print Successful")
 
